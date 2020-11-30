e4f7849217b955a3cdb0bfbe3e66bfd1911f566d
Ishaanis13 <69059812+Ishaanis13@users.noreply.github.com>
2020-11-30 16:55:41 -0500

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,54 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+      this.name = null;
+      this.rank = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+   static updateRank(rank1) {
+        database.ref('/').update({
+            Rank: rank1
+        })
+    }
+    getRank() {
+        var rankRef = database.ref("Rank");
+        rankRef.on("value", (data) => {
+            this.rank = data.val();
+        })
+    }
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+    resetDistance() {
+        
+        database.ref("players/player" + this.index).set({
+            distance: 0,
+            name : ""
+        })
+    }
+
+}
